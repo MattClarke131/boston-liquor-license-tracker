5f4a4b9b207f6ac92a7f9e9c6ba34d677c05cdd9
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-04-29 19:40:45 -0400

    Revert "add routing with tanstack router"
    This reverts commit 93828657479ac14837858ca80223d2a14a74b046.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/boston-liquor-license-tracker/",
-  plugins: [
-    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react()],
+  plugins: [react()],
 })
