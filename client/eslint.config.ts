689cbdf98e80439212bc7e61582997faff5fe5fc
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-10-06 21:10:54 -0400

    Update eslint formatting rules

--- client/eslint.config.ts
+++ client/eslint.config.ts
@@ -2,22 +2,39 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  // Typescript
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      globals: globals.browser
-    }
-  },
-
   // Recommended configs
   js.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+
+  // Typescript
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "indent": "off", // Use @typescript-eslint/indent instead
+      "@typescript-eslint/indent": ["error", 2],
+      "prefer-const": "error",
+      "no-var": "error",
+
+      // React
+      "react/self-closing-comp": "error",
+
+      // React Hooks
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    }
+  },
 
   // CSS
   {
