d0e3369b21c3ba5fc53d48fa0e083a0e97c3e59b
Matt Clarke <4278212+MattClarke131@users.noreply.github.com>
2025-10-06 22:04:50 -0400

    remove CSS while working through js issues

--- client/eslint.config.ts
+++ client/eslint.config.ts
@@ -3,7 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import css from "@eslint/css";
+// import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
